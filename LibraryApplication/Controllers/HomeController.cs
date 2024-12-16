a00e30e930ecb9dda4cd7b68bdb8839c6a7fc82f
oguz-gokcen <oguz.gokcen@mrusta.com.tr>
2024-12-17 01:43:50 +0300

    access denied page

--- LibraryApplication/Controllers/HomeController.cs
+++ LibraryApplication/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace LibraryApplication.Controllers
 			return View();
 		}
 
+		public IActionResult Denied()
+		{
+			return View();
+		}
+
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
